13ff47fc15544ea6c0de1b2f0d3089bee6d7c1ee
Sebailla <seba.illa.1977@gmail.com>
2025-09-19 13:35:46 -0300

    feat: enhance main application setup with static assets support and type safety

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { join } from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.useGlobalPipes(new ValidationPipe(
     {
       whitelist: true,
     }
   ))
+  app.useStaticAssets(join(__dirname, '../public')) // para uso de imagenes en la carpeta /public - localhost:3001/img/1.jpg
   await app.listen(process.env.PORT ?? 3001)
 }
 
